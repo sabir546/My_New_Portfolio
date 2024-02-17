af2bcd27d7d7a0c5080a60b94aadec56a81e6e37
sabir546 <mdgulamsabir9@gmail.com>
2024-02-17 17:21:22 +0530

    database updated

--- models/database.js
+++ models/database.js
@@ -1,6 +1,7 @@
 
 const mongoose = require('mongoose');
-mongoose.connect("mongodb+srv://mdgulamsabir9:1BQUzRsfi91dZPEq@new-portfolio.8yefuo6.mongodb.net/?retryWrites=true&w=majority")
+// mongoose.connect("mongodb+srv://mdgulamsabir9:1BQUzRsfi91dZPEq@new-portfolio.8yefuo6.mongodb.net/?retryWrites=true&w=majority")
+mongoose.connect("mongodb+srv://mdgulamsabir9:v20.9.0@cluster0.5dedhyw.mongodb.net/?retryWrites=true&w=majority")
 const portfolio = new mongoose.Schema({
     name: String,
     email:String,
