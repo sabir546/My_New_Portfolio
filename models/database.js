e25d11c5101d4cc9a3714b3ae865db597eb06bce
sabir546 <mdgulamsabir9@gmail.com>
2024-03-20 03:46:27 +0530

    added static mongodv

--- models/database.js
+++ models/database.js
@@ -1,8 +1,9 @@
 
 const mongoose = require('mongoose');
 
-// mongoose.connect("mongodb://127.0.0.1:27017/profoo")
-mongoose.connect("mongodb+srv://mdgulamsabir9:EK5_2sTCQ4s3kfD@portfolio.qimdmjs.mongodb.net/portfolio?retryWrites=true&w=majority")
+mongoose.connect("mongodb://127.0.0.1:27017/profoo")
+// mongoose.connect("mongodb+srv://mdgulamsabir9:EK5_2sTCQ4s3kfD@portfolio.qimdmjs.mongodb.net/portfolio?retryWrites=true&w=majority")
+// mongoose.connect("mongodb+srv://mdgulamsabir9:<password>@portfolio.qimdmjs.mongodb.net/?retryWrites=true&w=majority")
 const portfolio = mongoose.Schema({
     name: String,
     email:String,
