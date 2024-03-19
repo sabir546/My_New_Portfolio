e4ff0a02913b5afc27e775d963cbffc4d17a17d0
sabir546 <mdgulamsabir9@gmail.com>
2024-03-20 03:32:33 +0530

    verceladded

--- models/database.js
+++ models/database.js
@@ -1,8 +1,8 @@
 
 const mongoose = require('mongoose');
-//  mongoose.connect("mongodb+srv://mdgulamsabir9:dSjyQQn9YriDAPxJ@cluster3.ht3hvni.mongodb.net/?retryWrites=true&w=majority") 
-// mongoose.connect("mongodb+srv://mdgulamsabir9:dSjyQQn9YriDAPxJ@cluster3.ht3hvni.mongodb.net/")
-mongoose.connect("mongodb://127.0.0.1:27017/profoo")
+
+// mongoose.connect("mongodb://127.0.0.1:27017/profoo")
+mongoose.connect("mongodb+srv://mdgulamsabir9:EK5_2sTCQ4s3kfD@portfolio.qimdmjs.mongodb.net/portfolio?retryWrites=true&w=majority")
 const portfolio = mongoose.Schema({
     name: String,
     email:String,
